f3cd194ca7d7e6c92b8824e24d7c8f55d94c01aa
Luisfayre <luisfayre@hotmail.com>
2022-11-25 19:12:38 -0600

    Migracion -perfiles

---
+++ database/migrations/2022_11_26_010523_create_perfils_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('tipo');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfiles');
+    }
+}
